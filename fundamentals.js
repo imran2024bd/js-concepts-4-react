1b389a2901ab552b2905e91abe922fd4c90ba23b
imran2024bd <imranitbd34@gmail.com>
2024-05-20 17:26:08 +0600

    first commit

--- fundamentals.js
+++ fundamentals.js
@@ -14,5 +14,23 @@ else if (fatherName=== 'Arnold'){
 
 }
 else{
+
+}
+
+// 3. array declare
+// index, 
+// length, push, 
+const numbers = [89, 35 , 12, 98];
+numbers[0]=56;
+//console.log(numbers);
+
+// 4. for loop 
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+    console.log(number);
+}
+
+// 5. function
+function multiply(num1, num2) {
     
 }
