f016f5d25f9b36e94acfcff678cd7693ea9d4173
imran2024bd <imranitbd34@gmail.com>
2024-05-20 23:17:20 +0600

    First fundamental done

--- fundamentals.js
+++ fundamentals.js
@@ -27,10 +27,41 @@ numbers[0]=56;
 // 4. for loop 
 for (let i = 0; i < numbers.length; i++) {
     const number = numbers[i];
-    console.log(number);
+    // console.log(number);
 }
 
 // 5. function
 function multiply(num1, num2) {
-    
+    const result= num1 * num2;
+    return result;    
 }
+const output = multiply(35,78);
+// console.log(output);
+
+// 6. Object
+// 3 ways to access property by name 
+const student = {
+    name: 'sakib khan',
+    age: 32,
+    movies:['king khan', 'Dhakar Mastan', 'Aupu my Love']
+}
+
+const myVariable = 'age';
+
+console.log(student.age); // direct by property
+console.log(student['age']);  // access via property name string 
+console.log(student[myVariable]); // access via property name in a variable
+
+
+const mySelf = {
+    myName: ' Imran Hossain',
+    myAge: 38,
+    mySchoolname:'Gouripur',
+    fatherName:'Intaj ali',
+    fatherAge:78
+}
+const fatherProfile = 'fatherName';
+
+console.log(mySelf.myName); // direct by property
+console.log(mySelf['myAge']); // access via property name string 
+console.log(mySelf[fatherProfile]);  // access via property name in a variable
