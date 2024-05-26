00c230bbf4de131bf4bcfe8c89d72a369f210c11
imran2024bd <imranitbd34@gmail.com>
2024-05-26 15:53:52 +0600

    JSON, Fetch start

--- fetch.js
+++ fetch.js
@@ -1,24 +1,40 @@
+// 37-5 JSON, Fetch, Keys, Values, Array Add Or Remove Using Dots
+
+
 // 01 JSON  =>  stringify , parse
 const student = {
     name: 'Salib Khan',
     age: 32,
     movies: ['king khan', 'Dhakar Mastan']
-}
+};
+
+// -----------------------------------------------------------
+student.movies[0] = 'sakib khan';
+const stMovies = student.movies[0];
+// console.log(stMovies);
+
+
+// -----------------------------------------------------------
+
 
 // js object tr to JSON
 const studentJSON = JSON.stringify(student);
-console.log(student);
-console.log(studentJSON);
+// console.log(student);
+// console.log(studentJSON);
 
+const stJSON = JSON.stringify(student);
+// console.log(stJSON);
+// -----------------------------------------------------------
 
 // JSON to object
-
 const studentObj = JSON.parse(studentJSON);
-console.log(studentObj);
+// console.log(studentObj);
+const stObj = JSON.parse(stJSON);
+// console.log(stObj);
 
+// -----------------------------------------------------------
+// API stands for Application Programming Interface.
 // 2. fetch
-
-//2. fetch
 fetch('url')
-    .then(res => res.json())
-    .then(data => console.log(data));
+    .then(res => res.json());
+    .then ( data => console.log(data));
